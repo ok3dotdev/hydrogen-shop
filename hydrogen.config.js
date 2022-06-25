ab9916421399b7c3f9d87bc4cdb084d477f0da81
Henry Oke <hoke@LMG-00053.local>
2022-06-25 16:39:28 -0400

    Initial commit

---
+++ hydrogen.config.js
@@ -0,0 +1,36 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+// export default defineConfig({
+//   shopify: {
+//     defaultCountryCode: 'US',
+//     defaultLanguageCode: 'EN',
+//     storeDomain: 'hydrogen-preview.myshopify.com',
+//     storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+//     storefrontApiVersion: '2022-07',
+//   },
+//   session: CookieSessionStorage('__session', {
+//     path: '/',
+//     httpOnly: true,
+//     secure: import.meta.env.PROD,
+//     sameSite: 'Strict',
+//     maxAge: 60 * 60 * 24 * 30,
+//   }),
+// });
+
+export default defineConfig({
+    shopify: {
+      defaultCountryCode: 'US',
+      defaultLanguageCode: 'EN',
+      storeDomain: 'next-sanity.myshopify.com',
+      storefrontToken: '123f259320169dafeaab057c9f2c3d3e',
+      storefrontApiVersion: '2022-07',
+      showQueryTiming: true
+    },
+    session: CookieSessionStorage('__session', {
+      path: '/',
+      httpOnly: true,
+      secure: import.meta.env.PROD,
+      sameSite: 'Strict',
+      maxAge: 60 * 60 * 24 * 30,
+    }),
+  });
